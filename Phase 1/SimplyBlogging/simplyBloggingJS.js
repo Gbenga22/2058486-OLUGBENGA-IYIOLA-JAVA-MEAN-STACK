8b4fbaf265a84fd571dc431de5238ba04e59f8de
Olugbenga Iyiola <iyiolaolugbenga22@yahoo.com>
2021-08-06 18:07:41 -0600

    updated

--- Phase 1/SimplyBlogging/simplyBloggingJS.js
+++ Phase 1/SimplyBlogging/simplyBloggingJS.js
@@ -1,39 +1,27 @@
-//alert("Welcome to External JS")
+
 function addData() {
-    // alert("Event fired...")
-    //This code create p tag 
+
     var title = document.getElementById("title").value;
     var article = document.getElementById("article").value;
-
-    //document.getElementById("title").innerHTML
-    //document.getElementById("article").innerHTML
-
     var image = document.getElementById("image").value;
-    var img = document.createElement("img");
-    img.style.height = "200px";
-    img.style.width = "350px";
-    img.src = image;
+    var imgArray = new Array();
+    var emp = {Title:title, Article:article, Image:image};
+    imgArray.push(emp);
+   
+   
+   var TextImg = '';
 
-    var imgElement = document.createElement('img3');
-    var titleElement = document.createElement('img1');
-    var articleElement = document.createElement('img2');
-    
+$.each(imgArray, function(key, val) { 
+          TextImg = '<div class="txt_img"><img src="' + val.Image + '" class="img-thumbnail thumb m-r" width="300" height="300"/><span>' +"<br/>Title:  " +val.Title + '</span><span>' +"<br/>Description:  " +val.Article+ '</span></div>';
 
-    var myPTagContent = document.createTextNode("Blog Title is "+title)
-    var myPTagContent2 = document.createTextNode("Description is "+article)
-    
-    
-    //This code add text node to p tag 
-    titleElement.appendChild(myPTagContent);
-    articleElement.appendChild(myPTagContent2);
- 
-    // refer the tag using id selector and append the p tag to div tag
-    //document.getElementById("FirstBlog1").appendChild(titleElement);
-    //document.getElementById("FirstBlog2").appendChild(articleElement);
-    document.getElementById("FirstBlog1").innerHTML = "<br/> Blog Title is: "+ title +"<br/> Description of Article: "+article
-    //document.getElementById("FirstBlog2").innerHTML ="<br/> Description of Article: "+article
-    document.getElementById("FirstBlog2").appendChild(img);
-
-   // document.getElementById("obj1").innerHTML="<br> 2nd Statement";
+          $('#imageContainer').append(TextImg);
+});
+   
+   
+   
+   
+   
+   
+   
+   
 }
-
